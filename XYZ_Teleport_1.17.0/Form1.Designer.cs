d2439fd8787de30b56e55292bf09eccb7f10b23c
Laamy <User@DESKTOP-O86QES3>
2021-06-09 18:00:31 +1000

    Add project files.

---
+++ XYZ_Teleport_1.17.0/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace XYZ_Teleport_1._17._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(12, 38);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(12, 64);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDown3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Teleport";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(86, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Neg";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(86, 39);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Neg";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(86, 65);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(46, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Neg";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(135, 122);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Laamys teleporting plz stfu :cry:";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ XYZ_Teleport_1.17.0/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XYZ_Teleport_1._17._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Mem mem;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mem = new Mem();
+            mem.OpenProcess(mem.GetProcIdFromName("Minecraft.Windows.exe"));
+            teleport(
+                (float)numericUpDown1.Value,
+                (float)numericUpDown2.Value,
+                (float)numericUpDown3.Value);
+        }
+
+        string localPlayer = "Minecraft.Windows.exe+03FFFA98,0,50,138,";
+        int XHex = 0x4D0;
+
+        public void teleport(float x, float y, float z)
+        {
+            if (checkBox1.Checked)
+            {
+                mem.WriteMemory(localPlayer + XHex.ToString("X"), "float", (0 - x).ToString());
+                mem.WriteMemory(localPlayer + (XHex + 12).ToString("X"), "float", (0 - x + 0.6f).ToString());
+            }
+            else
+            {
+                mem.WriteMemory(localPlayer + XHex.ToString("X"), "float", x.ToString());
+                mem.WriteMemory(localPlayer + (XHex + 12).ToString("X"), "float", (x + 0.6f).ToString());
+            }
+
+            if (checkBox2.Checked)
+            {
+                mem.WriteMemory(localPlayer + (XHex + 4).ToString("X"), "float", (0 - y).ToString());
+                mem.WriteMemory(localPlayer + (XHex + 16).ToString("X"), "float", (0 - y + 0.6f).ToString());
+            }
+            else
+            {
+                mem.WriteMemory(localPlayer + (XHex + 4).ToString("X"), "float", y.ToString());
+                mem.WriteMemory(localPlayer + (XHex + 16).ToString("X"), "float", (y + 1.8f).ToString());
+            }
+
+            if (checkBox3.Checked)
+            {
+                mem.WriteMemory(localPlayer + (XHex + 8).ToString("X"), "float", (0 - z).ToString());
+                mem.WriteMemory(localPlayer + (XHex + 20).ToString("X"), "float", (0 - z + 0.6f).ToString());
+            }
+            else
+            {
+                mem.WriteMemory(localPlayer + (XHex + 8).ToString("X"), "float", z.ToString());
+                mem.WriteMemory(localPlayer + (XHex + 20).ToString("X"), "float", (z + 0.6f).ToString());
+            }
+        }
+    }
+}
