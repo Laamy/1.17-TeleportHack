f532477ce5028a0e58500aa1d14e4585bb8646e4
Laamy <mcpeconner0160@gmail.com>
2021-09-03 14:01:43 +1000

    Added some more offsets to the version class (Gamemode & Velocity)

--- XYZ_Teleport_1.17.0/VersionBase/VersionClass.cs
+++ XYZ_Teleport_1.17.0/VersionBase/VersionClass.cs
@@ -13,32 +13,41 @@ namespace XYZ_Teleport_1._17._0.VersionBase
             {
                 "1.",
                 "base+",
-                "" }
-            ),*/
+                "" ,
+                "" 
+            }),*/
             new IVersion(new string[] // might add Beta versions later :thinking: nah
             {
-                "1.17.11",
-                "base+041457D8,8,20,C8,",
-                "4D0" }
-            ),
+                "1.17.11", // Version
+                "base+041457D8,8,20,C8,", // LocalPlayer
+                "4D0", // PositionX-1 offset
+                "1E08", // Gamemode offset,
+                "50C" // VelocityX offset
+            }),
             new IVersion(new string[]
             {
                 "1.17.2",
                 "base+04020228,0,18,B8,",
-                "4D0" }
-            ),
+                "4D0" ,
+                "1E08",
+                "50C"
+            }),
             new IVersion(new string[]
             {
                 "1.17",
                 "base+03FFFA98,0,50,138,",
-                "4D0" }
-            ),
-            new IVersion(new string[]
+                "4D0",
+                "1E08",
+                "50C"
+            }),
+            new IVersion(new string[] // Leave a slot blank if you dont have the offset/dont know how to get it (I dont want to downgrade just to find the gamemode offset :p
             {
                 "1.16.221",
                 "base+03CDE520,38,50,140,",
-                "4A0" }
-            )
+                "4A0" ,
+                "",
+                "4E0"
+            })
         };
 
         public static void setVersion(IVersion version)
